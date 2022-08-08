03f1e2b5132c22ed63c2a37327e9312eb0ea6796
Amal K <12756149+1Amal@users.noreply.github.com>
2022-08-08 21:59:11 +1000

    08_08_2022 HTML, CSS and Python update
    Updated HTML description, CSS code, Python code display weather data

--- WeatherApp.py
+++ WeatherApp.py
@@ -13,8 +13,9 @@ def weather(location_input): # Define function
         api_address=f"https://api.openweathermap.org/data/2.5/weather?q={location_input}&appid={API_key}&units=metric" # Query to get the weather
         api_response=requests.get(api_address) # Save response onto variable
         api_to_dict=api_response.json() # Convert the API response into a Python Dictionary
-        current_temp=api_to_dict['main']['temp'] # Save the current temprature from the dictionary
-        return(f"Current Tempreature in {location_input} is: {current_temp}C") # Return this to function
+        current_temp=api_to_dict['main']["temp"] # Save the current temprature from the dictionary
+        weather_desc=api_to_dict["weather"][0]["description"]
+        return(f"Current temperature in {location_input}, is {current_temp} C, it is {weather_desc}") # Return this to function
 
 
 # Following function will declare the index function and call index.html and the weather () function
--- apitesting.py
+++ apitesting.py
@@ -6,9 +6,13 @@ def weather(location2):
         api_address=f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_key}&units=metric"
         api_response=requests.get(api_address)
         api_to_dict=api_response.json()
-        current_temp=api_to_dict['main']['temp']
-        print(f"current temp is:{current_temp}")
-        return()
+        current_temp=api_to_dict['main']
+        weather_desc=api_to_dict["weather"][0]["description"]
+
+        print(f"Current temp is:{current_temp}, {weather_desc}")
+        return(api_to_dict)
 
 weather_stat=weather("melbourne")
+# print(weather_stat["wind"])
+# print(weather_stat["weather"])
 print(weather_stat)
