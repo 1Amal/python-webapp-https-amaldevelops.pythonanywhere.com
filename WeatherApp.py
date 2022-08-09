142af68e0c931985e64e5555acb96ff47c86e376
Amal K <12756149+1Amal@users.noreply.github.com>
2022-08-09 22:32:26 +1000

    Update to Python, HTML, CSS
    Update to Python, HTML, CSS

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,21 +1,27 @@
-#Amal Kariyawasam, Assignment 3
+#Amal Kariyawasam, WeatherApp using OpenWeatherMap
 
 from flask import Flask, render_template, request # import what we need to use from the flask library
 import requests
-app = Flask(__name__) # invoke the Flask clas
+app = Flask(__name__) # invoke the Flask class
 #API Key: 4e0be172ba5affd1c6fb97aceabe2940 
 
 
-#Weather query function
+#Function to query Weather API and to return data 
 def weather(location_input): # Define function
         API_key="4e0be172ba5affd1c6fb97aceabe2940" # API Key from Openweather MAP
-        # location_input=location
         api_address=f"https://api.openweathermap.org/data/2.5/weather?q={location_input}&appid={API_key}&units=metric" # Query to get the weather
-        api_response=requests.get(api_address) # Save response onto variable
+        api_response=requests.get(api_address) # Save response into a variable
         api_to_dict=api_response.json() # Convert the API response into a Python Dictionary
-        current_temp=api_to_dict['main']["temp"] # Save the current temprature from the dictionary
         weather_desc=api_to_dict["weather"][0]["description"]
-        return(f"Current temperature in {location_input}, is {current_temp} C, it is {weather_desc}") # Return this to function
+        current_temp=api_to_dict["main"]["temp"] # Save the current temprature from the dictionary
+        weather_feels=api_to_dict["main"]["feels_like"]
+        pressure=api_to_dict["main"]["pressure"]
+        humidity=api_to_dict["main"]["humidity"]
+        selected_location=api_to_dict["name"]
+        selected_country=api_to_dict["sys"]["country"]
+
+
+        return(f"{selected_location}, {selected_country} is {weather_desc} and tempreature is:{current_temp} Degrees and it feels like {weather_feels} Degrees.\n Pressure is:{pressure} hpa and humidity is:{humidity}%, have an awesome day !") # Return this to function
 
 
 # Following function will declare the index function and call index.html and the weather () function
@@ -27,7 +33,6 @@ def main_route():
 @app.route("/<location>",methods=["GET"])
 def weather_by_location(location):
     return render_template("index.html",weather=weather(location))
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port="100", debug=True) # Start the server listening for requests
--- apitesting.py
+++ apitesting.py
@@ -1,18 +1,23 @@
 #Weather query
 import requests
-def weather(location2):
-        API_key="4e0be172ba5affd1c6fb97aceabe2940"
-        location="Melbourne,AU"
-        api_address=f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_key}&units=metric"
-        api_response=requests.get(api_address)
-        api_to_dict=api_response.json()
-        current_temp=api_to_dict['main']
+#Function to query Weather API and to return data 
+def weather(location_input): # Define function
+        API_key="4e0be172ba5affd1c6fb97aceabe2940" # API Key from Openweather MAP
+        api_address=f"https://api.openweathermap.org/data/2.5/weather?q={location_input}&appid={API_key}&units=metric" # Query to get the weather
+        api_response=requests.get(api_address) # Save response into a variable
+        api_to_dict=api_response.json() # Convert the API response into a Python Dictionary
         weather_desc=api_to_dict["weather"][0]["description"]
+        current_temp=api_to_dict["main"]["temp"] # Save the current temprature from the dictionary
+        weather_feels=api_to_dict["main"]["feels_like"]
+        pressure=api_to_dict["main"]["pressure"]
+        humidity=api_to_dict["main"]["humidity"]
+        selected_location=api_to_dict["name"]
+        selected_country=api_to_dict["sys"]["country"]
 
-        print(f"Current temp is:{current_temp}, {weather_desc}")
-        return(api_to_dict)
 
-weather_stat=weather("melbourne")
+        return(f"{selected_location}, {selected_country} is {weather_desc} and tempreature is:{current_temp} Degrees and it feels like {weather_feels} Degrees\n pressure is:{pressure} and humidity is:{humidity}, have an awesome day !") # Return this to function
+
+weather_stat=weather("melbourne,au")
 # print(weather_stat["wind"])
 # print(weather_stat["weather"])
 print(weather_stat)
